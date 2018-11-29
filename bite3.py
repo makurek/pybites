119fe763f35f12c732259303811dbae8b384b907
Marcin Kurek <md.kurek@gmail.com>
2018-11-29 14:16:39 +0100

    bite2

---
+++ bite3.py
@@ -0,0 +1,25 @@
+games_won = dict(sara=0, bob=1, tim=5, julian=3, jim=1)
+
+
+def my_print_game_stats(games_won=games_won):
+    """Loop through games_won's dict k, v pairs (items) and
+       print how many games each person has won, pluralize
+       'game' based on number"""
+    print(games_won)
+    for key, value in games_won.items():
+        if value == 1:
+            print(f"{key} has won {value} game")
+        else:
+            print(f"{key} has won {value} games")
+
+    pass
+
+def print_game_stats(games_won=games_won):
+    """Loop through games_won's dict k, v pairs (items) and
+       print how many games each person has won, pluralize
+       'game' based on number"""
+    for name, num_games in games_won.items():
+        games = "game" if num_games == 1 else "games"
+        print(f'{name} has won {num_games} {games}')
+
+print_game_stats()
