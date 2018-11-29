f0ccbdfe35851c02bff2112af66e79c70cc549dd
Marcin Kurek <md.kurek@gmail.com>
2018-11-29 13:57:49 +0100

    bite2

--- bite2.py
+++ bite2.py
@@ -1,7 +1,7 @@
 VALID_COLORS = ['blue', 'yellow', 'red']
 
 
-def print_colors():
+def my_print_colors():
     """Ask for color, lowercase it, check if 'quit' is entered, if so print
        'bye' and break, next check if given color is in VALID_COLORS, if not,
        continue, finally if that check passes, print the color"""
@@ -14,9 +14,27 @@ def print_colors():
         if inp not in VALID_COLORS:
             print("Not a valid color")
             continue
+    # this else is useless
         else:
             print(inp)
+
+    # thou shall not stop after a match!!!
             break
 
+def print_colors():
+    """Ask for color, lowercase it, check if 'quit' is entered, if so print
+       'bye' and break, next check if given color is in VALID_COLORS, if not,
+       continue, finally if that check passes, print the color"""
+    while True:
+        color = input('Enter a color: ').lower()
+        if color == 'quit':
+            print('bye')
+            break
+
+        if color not in VALID_COLORS:
+            print('Not a valid color')
+            continue
+
+        print(color)
 
 print_colors()
