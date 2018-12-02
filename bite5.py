6f22494c2bde4e6de8d7915ec5dcaf5bf5442949
Marcin Kurek <md.kurek@gmail.com>
2018-12-02 08:42:15 +0100

    pybites 4 & 5

--- bite5.py
+++ bite5.py
@@ -15,7 +15,7 @@ and now you know about slicing from the end as well :)
 keep enjoying our bites!
 """
 
-def slice_and_dice(text=text):
+def my_slice_and_dice(text=text):
     """Strip the whitespace (newlines) off text at both ends,
        split the text string on newline (\n).
        Next check if the first char of each (stripped) line is lowercase,
@@ -31,6 +31,26 @@ def slice_and_dice(text=text):
             item = item.rstrip('.')
             words = item.split(' ')
             results.append(words[words.__len__()-1])
+    return results
+
+def slice_and_dice(text=text):
+    """Strip the whitespace (newlines) off text at both ends,
+       split the text string on newline (\n).
+       Next check if the first char of each (stripped) line is lowercase,
+       if so split the line into words and append the last word to
+       the results list. Make sure the you strip off any trailing
+       exclamation marks (!) and dots (.), Return the results list."""
+    results = []
+    for line in text.strip().split('\n'):
+        line = line.strip()
+
+        if line[0] not in ascii_lowercase:
+            continue
+        words = line.split()
+
+        last_word_stripped = words[-1].rstrip('!.')
+        results.append(last_word_stripped)
+
     return results
 
 slice_and_dice(text)
