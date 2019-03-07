6a41a186443412284e28a5d89a66eaac5c7c6bf6
Marcin Kurek <md.kurek@gmail.com>
2019-03-07 06:19:25 +0100

    pybites 4 & 5

--- beginner-bite-21-query-a-nested-data-structure.py
+++ beginner-bite-21-query-a-nested-data-structure.py
@@ -1,4 +1,15 @@
 '''
+
+Given the provided cars dictionary:
+
+Get all Jeeps
+Get the first car of every manufacturer.
+Get all vehicles containing the string Trail in their names (should work for other grep too)
+Sort the models (values) alphabetically
+See the docstrings and tests for more details. Have fun!
+
+Update 18th of Sept 2018: as concluded in the forum it is better to pass the cars dict into each function to make its scope local.
+
 cars = {
     'Ford': ['Falcon', 'Focus', 'Festiva', 'Fairlane'],
     'Holden': ['Commodore', 'Captiva', 'Barina', 'Trailblazer'],
@@ -42,21 +53,39 @@ cars = {
 
 def get_all_jeeps(cars=cars):
     """return a comma  + space (', ') separated string of jeep models (original order)"""
-    pass
-
+    r = ', '.join(cars['Jeep'])
+    return r
 
 def get_first_model_each_manufacturer(cars=cars):
     """return a list of matching models (original ordering)"""
-    pass
+    r = []
+    for key, value in cars.items():
+        r.append(value[0])
+    return r
 
 
 def get_all_matching_models(cars=cars, grep='trail'):
     """return a list of all models containing the case insensitive
        'grep' string which defaults to 'trail' for this exercise,
        sort the resulting sequence alphabetically"""
-    pass
+    r = []
+    for key, value in cars.items():
+        for c in value:
+            if grep.lower() in c.lower():
+                r.append(c)
+    r.sort()
+    return r
+
+
 
 
 def sort_car_models(cars=cars):
     """sort the car models (values) and return the resulting cars dict"""
-    pass
+    for key, value in cars.items():
+        value.sort()
+    return cars
+
+get_all_jeeps(cars)
+get_first_model_each_manufacturer(cars)
+get_all_matching_models(cars)
+sort_car_models(cars)
