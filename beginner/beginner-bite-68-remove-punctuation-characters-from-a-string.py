0a38aee289e1f06b10332c33965b062e5b10feec
Marcin Kurek <md.kurek@gmail.com>
2019-07-29 07:49:58 +0200

    added bite 68

---
+++ beginner/beginner-bite-68-remove-punctuation-characters-from-a-string.py
@@ -0,0 +1,11 @@
+'''
+Complete remove_punctuation which receives an input string and strips out all
+punctuation characters (!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~).
+
+Return the resulting string. You can go full loop, list comprehension or maybe some nice stdlib functionality?
+'''
+
+
+def remove_punctuation(input_string):
+    """Return a str with punctuation chars stripped out"""
+    pass
