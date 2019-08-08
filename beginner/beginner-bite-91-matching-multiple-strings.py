843ad416b699e4dee9fa2e80dbdc8578142e45bf
makurek <md.kurek@gmail.com>
2019-08-08 11:50:39 +0200

    completed bite 91

--- beginner/beginner-bite-91-matching-multiple-strings.py
+++ beginner/beginner-bite-91-matching-multiple-strings.py
@@ -7,23 +7,35 @@ contains_any_py_chars, and contains_digits below. See more info in the docstring
 
 VOWELS = 'aeiou'
 PYTHON = 'python'
-
+import string
 
 def contains_only_vowels(input_str):
     """Receives input string and checks if all chars are
        VOWELS. Match is case insensitive."""
-    pass
-
+    lower = input_str.lower()
+    vowels = "aeoiu"
+    for c in lower:
+        if c not in vowels:
+            return False
+    return True
 
 def contains_any_py_chars(input_str):
     """Receives input string and checks if any of the PYTHON
        chars are in it. Match is case insensitive."""
-    pass
+    chars = "PYTHON".lower()
+    lower = input_str.lower()
+    for c in lower:
+        if c in chars:
+            return True
+    return False
 
 
 def contains_digits(input_str):
     """Receives input string and checks if it contains
        one or more digits."""
-    pass
+    for c in input_str:
+        if c in string.digits:
+            return True
+    return False
 
 
