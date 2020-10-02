3560702e6ef0778d88af343eb34eaeaa30782b7e
makurek <md.kurek@gmail.com>
2020-10-02 10:30:50 +0200

    refreshing bite-1

--- beginner/beginner-bite-1-sum-n-numbers.py
+++ beginner/beginner-bite-1-sum-n-numbers.py
@@ -1,5 +1,7 @@
 
 """
+TASK DESCRIPTION:
+
 Write a function that can sum up numbers:
 
 It should receive a list of n numbers.
@@ -8,10 +10,22 @@ Look closely to the type of the function's default argument ...
 """
 input = []
 
+############################### KEY TAKEAWAYS #################################
+###
+###
+###
+###
+###
+################################################################################
+
+
+### ----------- My solution ---------------------------
+
 def my_sum_numbers(numbers=None):
     result = 0
     # Dlaczego samo "if numbers:" nie dziala? Tzn. wtedy jak podamy pusta liste to nie spelnia warunku?
     # albo inaczej, przy przekazywaniu argumentu "if arg" dla pelnej tablicy dziala, dla pustej nie
+    # "false" values include False, None, 0 and [] (an empty list).
     if numbers != None:
         for item in numbers:
             result += item
@@ -20,10 +34,21 @@ def my_sum_numbers(numbers=None):
         result += i
     return result
 
-def sum_numbers_original(numbers=None):
+
+def test_sum(num=None):
+    return sum(num)
+
+
+### ---------- PyBites original solution ---------------
+
+def pyb_sum_numbers(numbers=None):
     if numbers is None:
         numbers = range(1, 101)
     return sum(numbers)
+
+### Tutaj nie wiem, co chcialem osiagnac. Chyba chcialem sprawdzic alternatywny sposob, jak moznaby
+### cos takiego napisac. Zamiast sprawdzac, co zostalo przekazane jako argument, mozna
+### probowac obsluzyc wyjatek 'NoneType' object is not iterable
 
 def sum_numbers_1(numbers=None):
     try:
@@ -32,4 +57,6 @@ def sum_numbers_1(numbers=None):
         return sum(range(1,101))
 
 
-print(sum_numbers(input))
+print(pyb_sum_numbers(input))
+
+print(test_sum())
