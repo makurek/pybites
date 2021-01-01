d5bc8d0af02289d75f817dd3db7d07e871b75365
makurek <md.kurek@gmail.com>
2021-01-01 19:08:03 +0100

    added intermediate bite 22, not done

--- beginner/beginner-bite-278-major-and-minor-numbers.py
+++ beginner/beginner-bite-278-major-and-minor-numbers.py
@@ -16,16 +16,21 @@ def major_n_minor(numbers):
     Input: an array with integer numbers
     Output: the majority and minority number
     """
-    ###https: // stackoverflow.com / questions / 55125342 / usage - of - dict - value - and -dict - key - in -python3
+    # https://stackoverflow.com/questions/55125342/usage-of-dict-value-and-dict-key-in-python3
     # https://stackoverflow.com/questions/52135345/is-some-dict-items-an-iterator-in-python
     # https://stackoverflow.com/questions/21062781/shortest-way-to-get-first-item-of-ordereddict-in-python-3
+    # https://stackoverflow.com/questions/49159238/why-does-the-kwargs-mapping-compare-equal-with-a-differently-ordered-ordereddi
+    # https://stackoverflow.com/questions/39980323/are-dictionaries-ordered-in-python-3-6
+    # https://stackoverflow.com/questions/50872498/will-ordereddict-become-redundant-in-python-3-7/
     test = {'oranges': 5,
             'apples': 3,
             'peach': 1}
     #print(type((test.items())))
     #print(type(test.values()))
     #print(type(test.keys()))
-    # Co to tak naprawde znaczy OrderedDict? ORdered wg klucza czy wartosci?
+    # Q: Co to tak naprawde znaczy OrderedDict? Ordered wg klucza czy wartosci?
+    # A: Zadne z powyzszego. Ordered znaczy, ze zachowana jest kolejnosc podczas wstawiania.
+
     d = OrderedDict()
     for num in numbers:
         try:
@@ -33,17 +38,15 @@ def major_n_minor(numbers):
         except:
             d[num] = 1
     #print(d)
-    zwykly = {}
+    dict = {}
     for num in numbers:
         try:
-            zwykly[num] += 1
+            dict[num] += 1
         except:
-            zwykly[num] = 1
-    sor = sorted(zwykly.items(), key=lambda x: x[1] )
-    print(sor)
+            dict[num] = 1
+    sort = sorted(dict.items(), key=lambda x: x[1] )
+    return sort[-1][0], sort[0][0]
     # difference between for i in d:
     # for i in d.items(): ?
-    #return (next((iter(reversed(sor.items()))))[0], next(iter(sor.items()))[0] )
-    return sor[-1][0], sor[0][0]
-
+    # return (next((iter(reversed(sor.items()))))[0], next(iter(sor.items()))[0] )
 print(major_n_minor([0,0,0,1,2,2]))
