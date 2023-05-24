7c92e633fdd56da158ed8da27660cc057678e15d
Marcin Kurek <marcin.kurek@play.pl>
2023-05-24 16:30:45 +0200

    refactoring

--- intro/intro-bite104-split-and-join.py
+++ intro/intro-bite104-split-and-join.py
@@ -1,3 +1,10 @@
+"""
+Split up the message on newline (\n) using the split builtin, then use the join builtin to stitch it together using a '|' (pipe).
+So Hello world:\nI am coding in Python :)\nHow awesome! would turn into: Hello world:|I am coding in Python :)|How awesome!
+Your code should work for other message strings as well.
+Note that as opposed to previous Intro Bites you need to return the new string!
+Also note that, although we wanted you to learn about split and join here, there are more ways to pull this one off (check out the forum upon resolving this Bite ...)
+"""
 
 message = """Hello world!
 We hope that you are learning a lot of Python.
@@ -8,9 +15,9 @@ Become a PyBites ninja!"""
 def split_in_columns(message=message):
     """Split the message by newline (\n) and join it together on '|'
        (pipe), return the obtained output"""
-    res = message.split('\n')
-    y = '|'.join(res)
-    return y
+    chunks = message.split('\n')
+    result = '|'.join(chunks)
+    return chunks
 
 
 
